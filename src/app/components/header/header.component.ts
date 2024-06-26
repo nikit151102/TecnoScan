8202d6cb4d730cc22a97a3750d825f36ff015a43
nikit15110295@gmail.com <60881026+nikit151102@users.noreply.github.com>
2024-06-26 09:56:58 +0700

    finish

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
+import { TieredMenuModule } from 'primeng/tieredmenu';
+import { ButtonModule } from 'primeng/button';
+import { MenuModule } from 'primeng/menu';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [ ButtonModule, MenuModule, ToastModule,TieredMenuModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  
+  items = [
+    {
+      label: 'Главная',
+      command: () => this.navigateTo('home')
+    },
+    {
+      label: 'Информация',
+      command: () => this.navigateTo('information')
+    },
+    {
+      label: 'Контакты',
+      command: () => this.navigateTo('contacts')
+    }
+  ];
+
   navigateTo(url: string): void {
     setTimeout(() => {
         const element = document.getElementById(url);
